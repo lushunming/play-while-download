2fa304afcec837d0f5307f85771166b8da7195c2
lushunming <1357197829@qq.com>
2025-08-25 08:42:13 +0800

    Publish version 1.0.0

--- composeApp/src/jvmMain/kotlin/cn/com/lushunming/Platform.kt
+++
@@ -1,7 +0,0 @@
-package cn.com.lushunming
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
--- composeApp/src/jvmMain/kotlin/cn/com/lushunming/models/Download.kt
+++
@@ -1,9 +0,0 @@
-package cn.com.lushunming.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Download(val url: String, val headers: MutableMap<String, String>, val filename: String)
-
-@Serializable
-data class Downloads(val list: MutableList<Download>)
